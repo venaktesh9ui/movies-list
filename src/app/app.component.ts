65aaaa8e3daaa2748602a387bcf9776f71f83931
venaktesh9ui <venkatesh9ui@gmail.com>
2019-12-20 07:48:46 +0530

    Add movie json file and movie component to show list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'movies-list';
+
+export class AppComponent implements OnInit {
+  public mylist = [];
+  public recommendations = [];
+
+  constructor(private http: HttpClient) {}
+
+  public ngOnInit() : void {
+    this.http.get('assets/data/movies.json').subscribe((response:any) => {
+      this.mylist = response.mylist;
+      this.recommendations = response.recommendations;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
